8b31e04632de11a9018b73586c4d9b551939170b
ooishiho <yuidacheng697@gmail.com>
2023-08-25 14:42:12 +0000

    [add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :customers
   get '/' => 'homes#top'
   get 'about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
