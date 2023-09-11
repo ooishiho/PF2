82d656838e14f7576bde9654f37a351ffe2c7df9
ooishiho <yuidacheng697@gmail.com>
2023-09-11 14:56:57 +0000

    [add]item

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
 
- root to: "public/homes#top"
-
   devise_for :customers,skip: [:passwords],controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+ root to: "public/homes#top"
+
   get '/home/about' => 'public/homes#about', as: 'about'
   get '/admin' => 'admin/homes#top'
+
+  namespace :admin do
+      resources :items, only: [:new]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
